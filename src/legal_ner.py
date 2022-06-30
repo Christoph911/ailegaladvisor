76247ad152d53729b9d279845adc5b7bd0a85b36
Christoph911 <christoph.hoppe1@gmx.de>
2022-06-30 08:45:33 +0000

    Changed path to NER Model

--- src/legal_ner.py
+++ src/legal_ner.py
@@ -1,10 +1,11 @@
 # -----------------------------------------------------------
 # Prototype of AI-based Document Retrieval and QA-System
 # V 0.1
-# (C) 2021 Christoph Hoppe, Germany
+# (C) 2022 Christoph Hoppe, Germany
 # Released under GNU Public License (GPL)
 # -----------------------------------------------------------
 
+import os 
 import streamlit as st
 import spacy
 from spacy_streamlit import visualize_ner
@@ -28,7 +29,7 @@ class LegalNER:
             DataFrame with Legal Named Entities
         '''
 
-        nlp = spacy.load(st.secrets.paths.ner_model)
+        nlp = spacy.load(os.path.join("..", "models", "Legal-NER"))
         doc = nlp(context)
         ner = visualize_ner(doc, labels=nlp.get_pipe("ner").labels)
 
